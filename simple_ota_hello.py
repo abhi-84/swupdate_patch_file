abdaf50553c46642d8eb294dd79bfc81c335f1d5
Abhishek kumar Srivastava <abhishek.centos@gmail.com>
2023-03-16 13:41:33 +0530

    Add files via upload

---
+++ simple_ota_hello.py
@@ -0,0 +1,3 @@
+print("1st update for U54 board")
+print("2nd update")
+
---
+++ updated_ota_hello.py
@@ -0,0 +1,5 @@
+print("1st update for U54 board")
+print("2nd update")
+print("3rd update for U54 board")
+print("4th update")
+
